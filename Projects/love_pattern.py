8b8cc0513d3c91b022ab9d91309fefadbada0a51
Subarna Poudel <subarnapdl@gmail.com>
2021-07-08 14:04:57 +0545

    8th July 2021

--- Projects/love_pattern.py
+++
@@ -1,5 +0,0 @@
-for r in range(6):
-    for cl in range(7):
-        print('*', end="") if (r == 0 and cl % 3 != 0) or (r == 1 and cl %
-                                                           3 == 0) or (r-cl == 2 or r+cl == 8) else print(" ", end="")
-    print("")
